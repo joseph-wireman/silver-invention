72e86fe8b9c902b7ef12e54d6142d9b994f8bf37
joseph-wireman <joewireman7198@gmail.com>
2021-07-05 09:35:45 -0400

    testing functionality

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-var robot = require(robotjs);
+// Type "Hello World" then press enter.
+var robot = require("robotjs");
 
-console.log("Hello");
+// Type "Hello World".
+robot.typeString("Hello World");
+
+// Press enter.
+robot.keyTap("enter");
