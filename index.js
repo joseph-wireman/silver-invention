7b0b07f65f1cb2b464492cdec783e85e840be789
joseph-wireman <joewireman7198@gmail.com>
2021-07-07 09:39:14 -0400

    added colors + pink logic

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // Type "Hello World" then press enter.
 var robot = require("robotjs");
 
+var test = 0;
+
 blueColors = [
     "4af0fe", "1b76f2", "14d8ff", "04d7fe", "05d7ff", "7b7ffe", "16c4f1", 
     "4c70f3", "4c71f4", "4b71f5", "2372eb", "91f3fb","01d8ff",
@@ -12,17 +14,19 @@ pinkColors = ["ff8dfb", "fe8cfb"];
 
 // while loop
 while(true) {
+    test++;
+    console.log(test);
     //console.log(robot.getPixelColor(333,378));
     if (blueColors.includes(robot.getPixelColor(333, 378))) {
         robot.keyTap("f");
+    } else if (pinkColors.includes(robot.getPixelColor(x,y))) {
+        robot.keyTap("j");
     }
 }
 
 
-//get color at spots where the notes hit
 
-//if color in spots is in blueColors/ pink colors array
-//press button
+
 
 //TODO
 //attach to gui 
